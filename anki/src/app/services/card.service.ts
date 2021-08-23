7614ebd6d36a5f6edd0bfb4f3e425073012367f2
unknown <william.nahirnei@hotmail.com>
2021-08-23 16:36:53 -0300

    implementação model e service User

--- anki/src/app/services/card.service.ts
+++ anki/src/app/services/card.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { User } from './../class/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardService {
+  private _user:User
 
   constructor() { }
+
+  public createUser(id:number,name:string,birthDate:Date){
+    this._user=new User(id,name,birthDate)
+  }
+
+  public getUser():User{
+    return this._user
+  }
+  public updateUser(name:string,birthDate:Date){
+    this._user.setName(name)
+    this._user.setBirthDate(birthDate)
+  }
 }
