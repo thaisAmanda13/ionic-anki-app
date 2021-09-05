cf6f3bc854dffe784528100bc73c66cafb5d68e3
carlosdanielpohlod <carlospohlod@gmail.com>
2021-09-04 22:15:32 -0300

    Adicionado uma pagina de cadastro de usario rascunho

--- anki/src/app/pages/cadastrar-card/cadastrar-card.module.ts
+++ anki/src/app/pages/cadastrar-card/cadastrar-card.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { CadastrarCardPageRoutingModule } from './cadastrar-card-routing.module';
@@ -11,6 +11,7 @@ import { CadastrarCardPage } from './cadastrar-card.page';
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     FormsModule,
     IonicModule,
     CadastrarCardPageRoutingModule
--- anki/src/app/pages/cadastrar-card/cadastrar-card.page.ts
+++ anki/src/app/pages/cadastrar-card/cadastrar-card.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Card} from '../../class/card'
+import {CardService} from '../../services/card.service'
 @Component({
   selector: 'app-cadastrar-card',
   templateUrl: './cadastrar-card.page.html',
@@ -7,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarCardPage implements OnInit {
   public _pergunta : string
-  public resposta : string
-  constructor() { }
+  public _resposta : string
+  public _dataRevisao : Date
+  public _dificuldade : string
+  public _categoriaCard : string
+  
+  public cadastrar(): void{
+    const data = new Date()
+    const dataRevisao = new Date(data.setDate(data.getDate())) 
+    const newCard = new Card(this._pergunta, this._resposta, false, dataRevisao, parseInt(this._dificuldade), parseInt(this._categoriaCard))
+    this._cardService.cadastrar(newCard)
+    console.log(newCard)
+    console.log("Cadastrado com sucesso")
+    console.log(this._cardService.getCardsRevisao())
+  }
+  constructor(private _cardService : CardService) { }
 
   ngOnInit() {
   }
