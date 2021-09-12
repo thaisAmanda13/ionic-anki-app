d42d4aa7dffb385085086b98748909bb8e9aaead
Thais Amanda <thais.amandas18@gmail.com>
2021-09-12 19:43:54 -0300

    Incluir alert de erro e sucesso

--- anki/src/app/pages/cadastro-baralho/cadastro-baralho.page.ts
+++ anki/src/app/pages/cadastro-baralho/cadastro-baralho.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BaralhoService } from './../../services/baralho.service';
 import { Router } from '@angular/router';
 import { Baralho } from 'src/app/class/Baralho';
+import { RegisterAlert } from 'src/app/alerts/registerAlert';
 
 @Component({
   selector: 'app-cadastro-baralho',
@@ -11,23 +12,26 @@ import { Baralho } from 'src/app/class/Baralho';
 export class CadastroBaralhoPage implements OnInit {
   private _nome: string;
   private _categoria: string;
-  constructor(private router: Router, private baralhoService: BaralhoService) { }
+  private _alert: RegisterAlert;
+  constructor(private router: Router, private baralhoService: BaralhoService) 
+  {
+    this._alert =  new RegisterAlert();
+  }
 
-
-  public cadastrar(): void{
-       
-        
+  public cadastrar(): void 
+  {
     if(this._nome!=undefined&&this._nome!=null&&this._nome.trim()!=""){
       if(this._categoria!=undefined&&this._categoria!=null&&this._categoria.trim()!=""){
-        this.baralhoService.cadastrar(this._nome,this._categoria)
-        this.redirect("baralhos")
+        this.baralhoService.cadastrar(this._nome,this._categoria);
+        this._alert.success("Baralho cadastrado com sucesso");
+        this.redirect("baralhos");
     }
     else{
-      alert("Categoria vazia")
+      this._alert.error("Categoria Vazia", "Todos os campos devem ser preenchidos!");
       }
     }
     else{
-      alert("Nome vazio")
+      this._alert.error("Campo NOME vazio", "Todos os campos devem ser preenchidos!");
     }
   }
 
