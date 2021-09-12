8fb284192a1eb62e55a5b0123c7ebc366c6fccdb
Thais Amanda <thais.amandas18@gmail.com>
2021-09-12 20:03:14 -0300

    alert incluso

--- anki/src/app/pages/editar-baralho/editar-baralho.page.ts
+++ anki/src/app/pages/editar-baralho/editar-baralho.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Baralho } from 'src/app/class/Baralho';
 import { BaralhoService } from './../../services/baralho.service';
+import { RegisterAlert } from 'src/app/alerts/registerAlert';
 
 @Component({
   selector: 'app-editar-baralho',
@@ -13,7 +14,12 @@ export class EditarBaralhoPage implements OnInit {
   private _nomeBaralho: string
   private _categoria: string
   private _baralho: Baralho
-  constructor(private _router: Router, private _baralhoService: BaralhoService) { }
+  private _alert: RegisterAlert
+
+  constructor(private _router: Router, private _baralhoService: BaralhoService) 
+  {
+    this._alert = new RegisterAlert();
+  }
 
   ngOnInit() {
     const nav = this._router.getCurrentNavigation()
@@ -31,15 +37,17 @@ export class EditarBaralhoPage implements OnInit {
     if (this._nomeBaralho != undefined && this._nomeBaralho != null && this._nomeBaralho.trim() != "") {
       if (this._categoria != undefined && this._categoria != null && this._categoria.trim() != "") {
         this._baralhoService.editar(this._idBaralho, this._nomeBaralho, this._categoria)
+        this._alert.success("Editado com sucesso")
         this.redirect("baralhos")
       }
-      else {
-        alert("Categoria vazia")
+      else{
+        this._alert.error("Erro ao editar", "Parece que você não preencheu o campo categoria")
       }
     }
     else {
-      alert("Nome vazio")
+      this._alert.error("Erro ao editar", "Parece que você não preencheu o campo nome")
     }
+
     this._baralho.setNome(this._nomeBaralho)
     this._baralho.setCategoria(this._categoria)
 
